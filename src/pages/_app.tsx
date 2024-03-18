8c7224df937b58a7ffab1d03ff48c2eb9cda1a36
Junita Berglin <junita.berglin@gmail.com>
2024-03-18 13:52:20 +0100

    ✨

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
-import { GlobalStyles } from "../components/Global/global-styles/GlobalStyles";
+import { GlobalStyles } from "../components/Global/global-styles/globalStyles";
 
 const MyPortfolioApp = ({ Component, pageProps }: AppProps) => {
   return (
